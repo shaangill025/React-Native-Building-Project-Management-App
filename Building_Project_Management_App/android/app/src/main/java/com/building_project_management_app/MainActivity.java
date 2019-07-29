142d52e10430c094afb8227db5135630ccf94da0
Shaanjot Gill <ssgill@gatech.edu>
2019-07-29 06:40:19 -0700

    Initial Commit

---
+++ Building_Project_Management_App/android/app/src/main/java/com/building_project_management_app/MainActivity.java
@@ -0,0 +1,15 @@
+package com.building_project_management_app;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "Building_Project_Management_App";
+    }
+}
